329a0f9a22b0987350fc9b2feed5247d9505b5dc
Stephen <stvlley.amzn@gmail.com>
2022-07-09 14:50:34 -0400

    stylling done finish modal for new notes entry

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import { AiOutlineControl, AiOutlineBarChart, AiOutlineSearch } from 'react-icons/ai'
+import { CgNotes } from 'react-icons/cg'
+import { RiMap2Line } from 'react-icons/ri'
+import { BiMessageDetail } from 'react-icons/bi'
+import { GiBatwingEmblem } from 'react-icons/gi'
+import { FiSearch } from 'react-icons/fi'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <section id='sidebar'>
+        <section className='title-container'>
+          <div className='top-icon'>
+            <GiBatwingEmblem />
+
+          </div>
+
+        </section>
+        <section className='links-container'>
+          <div className='links'>
+            <div className='icon'>
+              <AiOutlineControl />
+            </div>
+            <span>Dashboard</span>
+          </div>
+          <div className='links'>
+            <div className='icon'>
+              <AiOutlineBarChart />
+            </div>
+            <span>Analytics</span>
+          </div>
+          <div className='links'>
+            <div className='icon'>
+              <CgNotes />
+            </div>
+            <span>Logs</span>
+          </div>
+          <div className='links'>
+            <div className='icon'>
+              <BiMessageDetail />
+            </div>
+            <span>Messages</span>
+          </div>
+          <div className='links'>
+            <div className='icon'>
+              <RiMap2Line />
+            </div>
+            <span>Map</span>
+          </div>
+        </section>
+      </section>
+      <section id='main-content'>
+        <section id='top-section'>
+          <div className='input-box'>
+            <input placeholder='search...' />
+            <div id='search-icon'>
+              <FiSearch />
+            </div>
+          </div>
+          <span className='action-icons'>
+            <div className='action-icon-one'></div>
+            <div className='action-icon-two'></div>
+          </span>
+        </section>
+        <h1 id='welcome-title'>Welcome, Batman.</h1>
+        <section id='grid-container'>
+          <div className='grid-item one'>
+one
+          </div>
+          <div className='grid-item two'>
+two
+          </div>
+          <div className='grid-item three'>
+three
+          </div>
+          <div className='grid-item four'>
+four
+          </div>
+        </section>
+      </section>
+    </>
+  )
 }
 
-export default App;
+export default App
