6fae63d5dd3fee73e36973705c2ea6fc91629a7e
Stephen <stvlley.amzn@gmail.com>
2022-07-16 11:51:50 -0400

    syncfusion i choose yougit add .

--- src/components/Reports.js
+++ src/components/Reports.js
@@ -1,17 +1,39 @@
-import React, { useEffect, useState } from 'react'
+import { GridComponent, ColumnDirective, ColumnsDirective, Page, Inject, Group, Filter } from '@syncfusion/ej2-react-grids'
+import React, { useEffect, useState } from 'react';
+function Reports() {
 
-const Reports = () => {
-    const [reports, setReports] = useState([])
-    useEffect(() => {
-        fetch('https://data.austintexas.gov/resource/fdj4-gpfu.json')
-            .then(response => response.json())
-            .then(data => setReports(data))
-            .catch(err => console.error(err));
-    }, [])
-    console.log(reports)
-    return (
-        <nav className='reports-section'>{reports.map((report) => (<div>{report.crime_type}</div>))}</nav>
-    )
+  const [reports, setReports] = useState([])
+
+  useEffect(() => {
+    fetch('https://data.austintexas.gov/resource/fdj4-gpfu.json')
+      .then(response => response.json())
+      .then(data => setReports(data))
+      .catch(err => console.error(err));
+  }, [])
+  
+  console.log(reports)
+
+  return (
+    <div className="App">
+      <GridComponent
+      style={{ backgroundColor: '#00fbff'}} 
+      pageSettings={{ pageSize: 6}}
+      allowPaging={true}
+      allowGrouping={true} 
+      allowFiltering={true}
+      dataSource={reports}>
+        <ColumnsDirective>
+          <ColumnDirective field="incident_report_number" headerText='Report Number' textAlign='Left' width='100' />
+          <ColumnDirective field="crime_type" headerText='Description' textAlign='Left' width='100' />
+          <ColumnDirective field="occ_date" headerText='Date' textAlign='Left' width='100' />
+          <ColumnDirective field="occ_time" time headerText='Time' textAlign='Left' width='100' />
+          <ColumnDirective field="address" headerText='Address' textAlign='Left' width='100' />
+          <ColumnDirective field="zip_code" headerText='Zip Code' textAlign='Left' width='100' />
+        </ColumnsDirective>
+        <Inject services={[Page, Filter, Group]} />
+      </GridComponent>
+    </div>
+  );
 }
 
-export default Reports
+export default Reports;
